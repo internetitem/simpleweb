250a089d07f756a10702f1cec0e3ea99018a7acc
Adam Batkin <adam@batkin.net>
2013-04-15 00:06:18 -0400

    update test

--- source/test/com/internetitem/simpleweb/test/TestHandler.java
+++ source/test/com/internetitem/simpleweb/test/TestHandler.java
@@ -4,11 +4,10 @@ import javax.servlet.http.HttpServletRequest;
 
 import com.internetitem.simpleweb.response.StaticDataResponse;
 import com.internetitem.simpleweb.router.RequestHandler;
-import com.internetitem.simpleweb.router.Response;
 
 public class TestHandler implements RequestHandler {
 
-	public Response test1(HttpServletRequest request) {
+	public StaticDataResponse test1(HttpServletRequest request) {
 		return new StaticDataResponse("text/plain", "Hello World!");
 	}
 }
